17fcb4860eebe0eccdd6accb7b7454c6f82bcc22
UncleDrew23 <chenyingsd10@gmail.com>
2019-11-09 18:21:58 +0800

    超类、子类

---
+++ inheritance/src/inheritance/Manager.java
@@ -0,0 +1,38 @@
+package inheritance;
+
+/**
+ * @program: inheritance
+ * @description: Manager
+ * @author: ycbron
+ * @create: 2019-11-09 18:00
+ **/
+
+public class Manager extends Employee
+{
+    private double bonus;
+
+    /**
+     * @param name the employee's name
+     * @param salary the salary
+     * @param year the hire year
+     * @param month the hire month
+     * @param day the hire day
+     */
+    public Manager(String name, double salary, int year, int month, int day)
+    {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+
+    public double getSalary()
+    {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double b)
+    {
+        bonus = b;
+    }
+}
